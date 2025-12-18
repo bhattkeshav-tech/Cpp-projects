8a2906240b97819d4d1216d5f784dec767b8f4bd
Keshav bhatt <bhattkeshav2006@gmail.com>
2025-12-18 12:12:59 +0530

    added a sides of triangle question

---
+++ sides_of_triangle.cpp
@@ -0,0 +1,25 @@
+// Take 3 numbers input and check whether they can be the sides of a triangle or not
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a,b,c;
+    cout<<"Enter first side: ";
+    cin>>a; 
+    cout<<"Enter second side: ";
+    cin>>b;     
+    cout<<"Enter third side: ";
+    cin>>c;
+    if(a+b>c && b+c>a && c+a>b){
+        cout<<"The sides can form a triangle.";
+        }
+    else{
+        cout<<"The sides cannot form a triangle.";
+
+
+
+
+        }
+}
