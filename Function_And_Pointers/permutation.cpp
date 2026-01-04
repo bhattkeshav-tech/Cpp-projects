0d568de7150a79538d1e652318dc34dba3b20b00
Keshav bhatt <bhattkeshav2006@gmail.com>
2026-01-04 20:47:24 +0530

    added

---
+++ Function_And_Pointers/permutation.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+int factorial(int x){// function to calculate factorial of a number
+    int fact=1;
+    for(int i=1; i<=x;i++){ // loop from 1 to x
+        fact*=i;
+
+    }
+    return fact;
+}
+
+int main(){
+    int n ,r;
+
+    cout<<"Enter value of n:  ";
+    cin>>n;
+    cout<<"Enter value of r:  ";
+    cin>>r;
+
+    int nfact=factorial (n);
+
+    int rfact=factorial (r);
+
+    int nminusrfact=factorial (n-r);
+
+    cout<<"nPr is: "<< nfact/(nminusrfact)<<endl;
+      
+    
+        //formula for nPr = n! / (n-r)!
+
+    return 0;
+  
+  }
+
+
---
+++ Recursion/function_Calling_Itself.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+void print(int n){
+    if(n==0) return;
+    cout<<x<<endl;
+    print(n-1);
+    
+  }
+
+int main(){
+print(5);
+
+
+
+
+
+}
